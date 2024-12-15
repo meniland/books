d876d07b9cb5d7a489d1ec8f7686d3e97d487e31
mlandman <mlandman@golantelecom.co.il>
2024-12-15 10:53:47 +0200

    Handled environments variables

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const dotenv = require('dotenv');
+require('dotenv').config();
 const PORT = 5000;
-const MONGO_URI = 'mongodb+srv://mlandman:meni8633@cluster0.ws6nf.mongodb.net/books?retryWrites=true&w=majority&appName=Cluster0';
+const MONGO_URI = process.env.MONGO_URI;
 
 const usersRouter = require('./routes/usersRoutes');
 const itemsRouter = require('./routes/itemsRoutes');
@@ -13,7 +13,6 @@ const authRouter = require('./routes/authRoutes');
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('/app/config/uploads'));
-dotenv.config({ path: './config.env' });
 
 mongoose.connect(MONGO_URI)
    .then(() => console.log('MongoDB connected'))
